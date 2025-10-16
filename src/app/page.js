508cc9ddf57c1c5c3637d99fb740d07de6b0542c
Evan GRUCHOT <evangruchot.pro@gmail.com>
2025-10-16 14:14:50 +0200

    update tft rank

--- src/app/page.js
+++ src/app/page.js
@@ -131,13 +131,13 @@ export default function Home() {
 
                         <div className={styles.contentContainer}>
                             <div className={styles.contentRank}>
-                                <p>PLATINUM IV</p>
+                                <p>PLATINUM III</p>
                                 <p>-</p>
-                                <p className={styles.textBlue}>71 LP</p>
+                                <p className={styles.textBlue}>22 LP</p>
                             </div>
 
                             <p className={styles.contentTotal}>
-                                {getTotalLP('PLATINUM', 'IV', 71) + ' / 2000 LP'}
+                                {getTotalLP('PLATINUM', 'III', 22) + ' / 2000 LP'}
                             </p>
                         </div>
                     </div>
